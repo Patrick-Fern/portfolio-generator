99d31f9ad47cc9af1af6b1350c10efb9835287fd
patfern <patrick.j.fernbach@gmail.com>
2022-02-12 12:33:02 -0500

    generate html page and code refactor

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs')
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArgs.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('===========');
+const [name, github] = profileDataArgs;
 
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-        
-    });
-    
-};
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
